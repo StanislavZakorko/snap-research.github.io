d0c0937cd4dd7df2ed293e57e3565145be5448e8
StanislavZakorko <staraley1986>
2021-05-25 13:34:05 +0300

    add events lib

---
+++ js/events-lib.js
@@ -0,0 +1,27 @@
+const eventsList = [
+  {
+    id: 0,
+    date: 'March 15, 2021',
+    eventName: 'Lorem ipsum dolor set',
+    eventShort: 'ICWSM 2021',
+  },
+  {
+    id: 1,
+    date: 'February 08, 2021',
+    eventName: 'Lorem ipsum dolor set',
+    eventShort: 'WWW 2021',
+  },
+  {
+    id: 2,
+    date: 'January 04, 2021',
+    eventName: 'Lorem ipsum dolor set',
+    eventShort: 'CHI 2021',
+  },
+  {
+    id: 3,
+    date: 'December 05, 2020',
+    eventName: 'Lorem ipsum dolor set',
+    eventShort: 'AIES 2021',
+  },
+
+];
