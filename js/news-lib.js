90abdac11b13d2bb38dad8872eb813d0a3a08495
bohdanhalaiko <bohdan.halaiko@gmail.com>
2021-05-26 19:21:10 +0300

    added meta tags

--- js/news-lib.js
+++ js/news-lib.js
@@ -3,12 +3,12 @@ const newsList = [
     id: 0,
     date: "December 21, 2020",
     link: "../news.html",
-    slug: "2020-snap-research-fellows",
+    slug: "2020-snap-research-fellowship",
     title: "2020 Snap Research Fellowship",
     descriptionShort: "Congratulations to our 2020 Snap Research Fellows!",
     description: "../news/detail/2020-snap-research-fellows.html",
-    metaTitle: "CCongratulations to our 2020 Snap Research Fellows!",
-    metaDescription: "",
+    metaTitle: "Congratulations to our 2020 Snap Research Fellows!",
+    metaDescription: "We are pleased to announce the recipients of our 2020 Snap Research Fellowships!  Snap Research Fellowship program is designed to honor the brightest, most driven students who positively impacted research and product development during their internships at Snap. We aim to provide students additional resources and freedom to pursue their innovative ideas, helping shape them into tangible cutting-edge research projects and products. Snap Research Fellows receive a $10,000 academic award.",
   },
   {
     id: 1,
@@ -21,7 +21,7 @@ const newsList = [
     description:
       "../news/detail/snap-research-wins-best-in-show-award-at-siggraph-2020",
     metaTitle: "Snap Research Wins “Best in Show” Award at SIGGRAPH 2020",
-    metaDescription: "",
+    metaDescription: "Snap Research’s Creative Vision team won the “Best in Show” award at SIGGRAPH 2020 Real Time Live for their demo, “Interactive Style Transfer to Live Video Streams.” The technology is the result of a collaboration between Senior Research Scientist Menglei Chai, former Research Intern Ondřej Texler, and Lead Research Scientist Sergey Tulyakov.",
   },
   {
     id: 2,
@@ -35,7 +35,7 @@ const newsList = [
     description: "../news/detail/snap-creative-challenge-acm-imx-2020",
     metaTitle:
       "Snap Creative Challenge Invites Universities to Reimagine AR Storytelling",
-    metaDescription: "",
+    metaDescription: "What will storytelling look like in a world where AR is more prevalent? Snap Research organized a virtual Creative Challenge at ACM IMX, an academic conference focused on interactive media and television, to explore that exact question.",
   },
 ];
 
